acd215e40a8bec504ee2e579f782f03f6100048a
BenS <bshalev1@gmail.com>
2024-01-08 16:25:56 +0200

    home work

--- UniqueChars.java
+++ UniqueChars.java
@@ -2,7 +2,6 @@
 public class UniqueChars {
     public static void main(String[] args) {  
         String str = args[0];
-        System.out.println(str.length());
         System.out.println(uniqueChars(str));
     }
 
